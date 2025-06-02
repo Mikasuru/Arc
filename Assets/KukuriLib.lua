d926dfa5d4fa3a09d429c530724b45a2219356f3
Kukuri and the Harmonic Odyssey <83948007+Mikasuru@users.noreply.github.com>
2025-06-03 01:03:38 +0700

    Update KukuriLib.lua

--- Assets/KukuriLib.lua
+++ Assets/KukuriLib.lua
@@ -603,7 +603,7 @@ local Colors = {
 
             local sliderTrackDrawingWidth = numSegments * segmentVisualWidth + (numSegments - 1) * segmentVisualSpacing
 
-            local animationUpdateIntervalFrames = 5 -- high = slow
+            local animationUpdateIntervalFrames = 10 -- high = slow
             local animationFrameCounter = 0
 
             local sliderTrackDisplay = Instance.new("Frame")
